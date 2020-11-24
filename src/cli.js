70e73d492562b331659ad24bc76f58c0f9bd3220
ldolohov <collapsemetoo@gmail.com>
2020-11-24 22:33:03 +0900

    prepare to git action

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const userName = () => {
+const userName = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-}
+};
+export default userName;
