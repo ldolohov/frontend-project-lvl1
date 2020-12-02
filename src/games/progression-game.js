5b9f925d8890414b108065bfdab397ba0cd22db6
ldolohov <collapsemetoo@gmail.com>
2020-12-02 18:40:39 +0900

    changed logic og result const

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -1,21 +1,22 @@
 import getRandomNumber from '../randomNumber.js';
+import gameFlow from '../index.js';
 
-export const gameDescription = 'What number is missing in the progression?';
+const gameDescription = 'What number is missing in the progression?';
 
-export const progressionGame = () => {
+const progressionGame = () => {
   const arr = [];
   let startNum = getRandomNumber(2);
   const progressNum = getRandomNumber(2, 10);
+  const hiddenElement = getRandomNumber(0, 9);
   for (let i = 0; i < 10; i += 1) {
-    arr.push(startNum);
-    startNum += progressNum;
+    arr.push(i === hiddenElement ? '..' : i * progressNum + startNum);
   }
-  const result = arr[getRandomNumber(0, 9)];
-  const indexOfRandom = arr.indexOf(result);
-  arr[indexOfRandom] = '..';
-  const question = `${arr}`;
+  const result = String(hiddenElement * progressNum + startNum);
+  const question = arr.join(' ');
   return {
     question,
     result,
   };
 };
+
+export default () => gameFlow(gameDescription, progressionGame);
